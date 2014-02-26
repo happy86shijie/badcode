0e0001eeb7198af014dea7c06decf8bf360f2ce9
root <root@ubuntu.(none)>
2014-02-26 18:14:09 +0800

    scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot1::Application.config.secret_key_base = '1d0c5c9fcf3e651923e72c60ee1c2db897c904a83ada9fe2ea56e42e04212610fc8e63a00623cd5314f72728b3c8cd263f61c956364d4548c6c59d67251229c1'
